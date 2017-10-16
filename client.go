be5a1be417c1dbc538eb4087d2bcc5d85e16f3b8
Georgi N. Georgiev <gngeorgiev.it@gmail.com>
2017-10-16 12:04:37 +0300

    fixed DisablePinging will block because stopPinging channel was not created

--- client.go
+++ client.go
@@ -128,6 +128,7 @@ func NewClient(certificate tls.Certificate) *Client {
 	}
 	client.Certificate = certificate
 	client.Host = DefaultHost
+	client.stopPinging = make(chan struct{})
 
 	return client
 }
